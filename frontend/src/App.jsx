b805a045184219d906b29093b683557dca4a54e8
Navigoo <...>
2024-05-08 06:39:38 +0200

    Add first version

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+
+    useEffect(() => {
+        fetch('/api')
+          .then((response) => response.json())
+          .then((result) => {
+            alert(`Hello ${result.hello}!`)
+          })
+      }, [])
+
+
+  return (
+    <>
+
+
+
+    </>
+  )
+}
+
+export default App
